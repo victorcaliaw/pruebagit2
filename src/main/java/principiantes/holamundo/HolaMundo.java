a37050367cb8571cbdece54eb1392af8b9aada3d
victorcaliaw <vicmovil@gmail.com>
2024-03-29 20:19:31 +0100

    probandoHolaMundo

---
+++ src/main/java/principiantes/holamundo/HolaMundo.java
@@ -0,0 +1,10 @@
+package principiantes.holamundo;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+        System.out.println("Todo bien");
+      
+    }
+}
